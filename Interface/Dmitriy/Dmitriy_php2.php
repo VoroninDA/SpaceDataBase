af81d1075cdf764f02e2a3dfb9b206d1756b0170
Dmitriy Voronin <Voroninda@vcu.edu>
2017-04-29 23:13:23 -0400

    Dmitriy plays with php

--- Interface/Dmitriy/Dmitriy_php2.php
+++ Interface/Dmitriy/Dmitriy_php2.php
@@ -6,9 +6,62 @@
 	</head>
 
 	<body>
+
+		<?php 
 	
-	<?php echo "<p>Data processed</>" ; ?>
+	 "<p>Data processed</>
+<p/>" ; 
+	date_default_timezone_set('UTC');
+	echo date('h:i:s:u a, l F jS Y e');
 	
+	
+	 /* Echos the date
+
+                h : 12 hr format
+
+             H : 24 hr format
+
+                i : Minutes
+
+                s : Seconds
+
+              u : Microseconds
+
+                a : Lowercase am or pm
+
+                l : Full text for the day
+
+                F : Full text for the month
+
+                j : Day of the month
+
+                S : Suffix for the day st, nd, rd, etc.
+                Y : 4 digit year
+
+	*/
+	
+	//$variablename
+	//['what is being pulled']
+	//$_POST Pulls from html vars
+	
+	//$_POST['VARNAME'];
+	
+	$usersName = $_POST['username'];
+	$street =$_POST['address'];
+	$city = $_POST['city'];
+	
+	// DOT ( . ) is string concatination
+	
+	echo $usersName . "</br>";
+	echo $street . "</br>";
+	echo $city . "</br>";
+	
+	
+	
+	?>
+	
+	
+
 	</body>
-	
+
 </html>
